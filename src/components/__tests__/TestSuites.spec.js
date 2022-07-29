9981056c0222811b39e7475a80d37667ddfe78e4
Travis <travis@localhost.dev>
2022-07-29 12:15:41 +1000

    basic snapshot test

---
+++ src/components/__tests__/TestSuites.spec.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { TestSuites } from "../TestSuites";
+
+describe("TestSuites", () => {
+  let comp, props;
+
+  describe("when empty", () => {
+    props = { testSuites: [] };
+    comp = renderer.create(<TestSuites {...props} />);
+  });
+
+  it("renders", () => {
+    expect(comp).toMatchSnapshot();
+  });
+});
